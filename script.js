91a65b69b5c8042e8556574be405f8a0e92605ba
TheLandlordOfPickles <dinsbeiners2@gmail.com>
2024-02-07 16:00:59 +0200

    Added: 1. Abilty to change movement speed. 2. Pressing r resets the camera position.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //  https://www.toptal.com/developers/keycode
 
-document.addEventListener("keydown", movement)
+document.addEventListener("keydown", whenButtonIsPressed)
 
 var world = document.getElementById('world')
+
+//Kustības ātruma lielumi
+var movementSpeed = 20
+var setMovementSpeed
 
 //Kustības koordinātes
 let x_dir = 500
@@ -15,57 +19,82 @@ let y_rot = 0
 let z_rot = 0
 
 
-function movement() {
+function whenButtonIsPressed() {
+    // r taustiņš restartē pozīciju/atrašanās vietu
+    if (event.keyCode == 82) {
+        x_dir = 500
+        z_dir = 0
+        y_dir = 250
+        x_rot = -30
+        y_rot = 0
+        z_rot = 0
+    }
+
+
+    //Kustības ātrums
+    // p tautsiņš dos lodziņu ātruma maiņai
+    if (event.keyCode == 80) {
+        setMovementSpeed = Number(prompt("Enter a number to change movement speed.", "20"))
+        if (!isNaN(setMovementSpeed)) {
+            movementSpeed = setMovementSpeed
+        } else {
+            alert("Please enter a number!")
+        }
+        console.log("Movement Speed: " + movementSpeed)
+        console.log("Set movement Speed: " + setMovementSpeed)
+    }
+
+    
     //Kustība
     // d taustiņš
     if (event.keyCode == 68) {
-        x_dir -= 10
+        x_dir -= movementSpeed
     }
     // a taustiņš
     if (event.keyCode == 65) {
-        x_dir += 10
+        x_dir += movementSpeed
     }
     // w taustiņš
     if (event.keyCode == 87) {
-        z_dir += 10
+        z_dir += movementSpeed
     }
     // s taustiņš
     if (event.keyCode == 83) {
-        z_dir -= 10
+        z_dir -= movementSpeed
     }
     // spacebar taustiņš
     if (event.keyCode == 32) {
-        y_dir += 10
+        y_dir += movementSpeed
     }
     // left_shift taustiņš
     if (event.keyCode == 16) {
-        y_dir -= 10
+        y_dir -= movementSpeed
     }
 
     //Rotācija
     // up_arrow taustiņš
     if (event.keyCode == 38) {
-        x_rot -= 10
+        x_rot -= movementSpeed
     }
     // down_arrow taustiņš
     if (event.keyCode == 40) {
-        x_rot += 10
+        x_rot += movementSpeed
     }
     // e taustiņš
     if (event.keyCode == 69) {
-        y_rot += 10
+        y_rot += movementSpeed
     }
     // q taustiņš
     if (event.keyCode == 81) {
-        y_rot -= 10
+        y_rot -= movementSpeed
     }
     // right_arrow taustiņš
     if (event.keyCode == 39) {
-        z_rot += 10
+        z_rot += movementSpeed
     }
     // left_arrow taustiņš
     if (event.keyCode == 37) {
-        z_rot -= 10
+        z_rot -= movementSpeed
     }
 }
 
