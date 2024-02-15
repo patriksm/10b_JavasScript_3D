bc246f4021dd0f902ab33b7afd58d00bfa42302d
arnisba <arnis.bandenieks@gmail.com>
2024-02-15 13:09:12 +0200

    saule

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const infoWindow = document.getElementById('infoWindow')
 
 const coin = document.getElementById( "euro" )
 const coin2 = document.getElementById( "euro2" )
+const saule = document.getElementById( "saule" )
 
 const deg = Math.PI / 180
 const accselFreeFall = 9.8066
@@ -95,6 +96,7 @@ function updateWorld() {
 
         coin.style.transform = `translate3d(200px, 100px, 100px) rotateX(0deg) rotateY(${ cointRotY }deg) rotateZ(0deg)`
         coin2.style.transform = `translate3d(-100px, 100px, 100px) rotateX(0deg) rotateY(${ cointRotY }deg) rotateZ(0deg)`
+        saule.style.transform = `translate3d(0px, -1000px, 400px) rotateX(0deg) rotateY(${ rotation.y }) rotateZ(0deg);`
 }
 
 function updatePlayerMovement() {
